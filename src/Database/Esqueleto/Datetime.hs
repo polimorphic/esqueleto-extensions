10f936534b464db68790dad9ac69733c2f86fd43
Parth Shah <shahp@mit.edu>
2021-08-12 21:43:50 -0700

    add dateTruncM for maybe values

--- src/Database/Esqueleto/Datetime.hs
+++ src/Database/Esqueleto/Datetime.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE MultiParamTypeClasses, OverloadedStrings #-}
 
-module Database.Esqueleto.Datetime (dateTrunc, generateSeries) where
+module Database.Esqueleto.Datetime (dateTrunc, dateTruncM, generateSeries) where
 
 import Data.Time (UTCTime)
 import Database.Esqueleto (SqlExpr, SqlString, SqlQuery, Value)
@@ -10,6 +10,12 @@ dateTrunc :: (SqlString a)
            => SqlExpr (Value a) -> SqlExpr (Value UTCTime) -> SqlExpr (Value UTCTime)
 dateTrunc a b = unsafeSqlFunction "date_trunc" (a, b)
 infixl 2 `dateTrunc`
+
+dateTruncM :: (SqlString a)
+           => SqlExpr (Value a) -> SqlExpr (Value (Maybe UTCTime))
+           -> SqlExpr (Value (Maybe UTCTime))
+dateTruncM a b = unsafeSqlFunction "date_trunc" (a, b)
+infixl 2 `dateTruncM`
 
 generateSeries ::(SqlString a) =>  SqlExpr (Value UTCTime) -> SqlExpr (Value UTCTime)
                -> SqlExpr (Value a) -> SqlQuery (SqlExpr (Value UTCTime))
