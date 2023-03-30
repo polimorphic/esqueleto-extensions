53351668c022cc760b7a1740841110ff86a4e677
Parth Shah <parth@polimorphic.com>
2023-03-30 12:32:21 -0700

    fix extract epoch

--- src/Database/Esqueleto/Datetime.hs
+++ src/Database/Esqueleto/Datetime.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE MultiParamTypeClasses, OverloadedStrings, TupleSections #-}
+{-# LANGUAGE MultiParamTypeClasses, OverloadedStrings #-}
 
-module Database.Esqueleto.Datetime (dateTrunc, dateTruncM, extraEpoch, generateSeries) where
+module Database.Esqueleto.Datetime (dateTrunc, dateTruncM, extractEpoch, generateSeries) where
 
 import Data.Time (UTCTime)
 import Database.Esqueleto (SqlExpr, SqlString, SqlQuery, Value)
-import Database.Esqueleto.Internal.Internal (unsafeSqlCastAs, unsafeSqlFunction)
+import Database.Esqueleto.Internal.Internal (unsafeSqlBinOp, unsafeSqlCastAs, unsafeSqlFunction)
 
 dateTrunc :: (SqlString a)
            => SqlExpr (Value a) -> SqlExpr (Value UTCTime) -> SqlExpr (Value UTCTime)
@@ -18,7 +18,7 @@ dateTruncM a b = unsafeSqlFunction "date_trunc" (a, b)
 infixl 2 `dateTruncM`
 
 extractEpoch :: SqlExpr (Value UTCTime) -> SqlExpr (Value Int)
-extractEpoch = unsafeSqlFunction "extract" . ("epoch" :: SqlExpr (Value String),)
+extractEpoch t = unsafeSqlFunction "extract" (unsafeSqlBinOp " from " "epoch" t)
 
 generateSeries ::(SqlString a) =>  SqlExpr (Value UTCTime) -> SqlExpr (Value UTCTime)
                -> SqlExpr (Value a) -> SqlQuery (SqlExpr (Value UTCTime))
